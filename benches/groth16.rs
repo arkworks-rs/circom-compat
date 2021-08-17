8f328e21e4d88d73457409d575e4b54b6cd7a589
Georgios Konstantopoulos <me@gakonst.com>
2021-08-17 14:56:39 +0300

    bench: use pre-calculated matrices/constraints

--- benches/groth16.rs
+++ benches/groth16.rs
@@ -1,41 +1,74 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, black_box};
 
-use ark_circom::{CircomBuilder, CircomConfig};
+use ark_circom::{read_zkey, CircomReduction, WitnessCalculator};
 use ark_std::rand::thread_rng;
 
 use ark_bn254::Bn254;
-use ark_groth16::{
-    create_random_proof as prove, generate_random_parameters, prepare_verifying_key, verify_proof,
-};
+use ark_groth16::{create_proof_with_qap_and_matrices, prepare_verifying_key, verify_proof};
 
+use std::{collections::HashMap, fs::File};
 
 fn groth(c: &mut Criterion) {
-    let cfg = CircomConfig::<Bn254>::new(
-        "./test-vectors/complex-circuit.wasm",
-        "./test-vectors/complex-circuit.r1cs",
-    ).unwrap();
+    let path = "./test-vectors/test.zkey";
+    let mut file = File::open(path).unwrap();
+    let (params, matrices) = read_zkey(&mut file).unwrap();
+    let num_inputs = matrices.num_instance_variables;
+    let num_constraints = matrices.num_constraints;
 
-    let mut builder = CircomBuilder::new(cfg);
-    builder.push_input("a", 3);
+    let inputs = {
+        let mut inputs: HashMap<String, Vec<num_bigint::BigInt>> = HashMap::new();
+        let values = inputs.entry("a".to_string()).or_insert_with(Vec::new);
+        values.push(3.into());
 
-    // create an empty instance for setting it up
-    let circom = builder.setup();
+        let values = inputs.entry("b".to_string()).or_insert_with(Vec::new);
+        values.push(11.into());
+
+        inputs
+    };
+
+    let mut wtns = WitnessCalculator::new("./test-vectors/mycircuit.wasm").unwrap();
+    let full_assignment = wtns
+        .calculate_witness_element::<Bn254, _>(inputs, false)
+        .unwrap();
 
     let mut rng = thread_rng();
-    let params = generate_random_parameters::<Bn254, _, _>(circom, &mut rng).unwrap();
+    use ark_std::UniformRand;
+    let rng = &mut rng;
 
-    let circom = builder.build().unwrap();
+    let r = ark_bn254::Fr::rand(rng);
+    let s = ark_bn254::Fr::rand(rng);
 
-    let inputs = circom.get_public_inputs().unwrap();
+    let proof = create_proof_with_qap_and_matrices::<_, CircomReduction>(
+        &params,
+        r,
+        s,
+        &matrices,
+        num_inputs,
+        num_constraints,
+        full_assignment.as_slice(),
+    )
+    .unwrap();
 
-    let proof = prove(circom.clone(), &params, &mut rng).unwrap();
     let pvk = prepare_verifying_key(&params.vk);
-    let verified = verify_proof(&pvk, &proof, &inputs).unwrap();
+    let inputs = &full_assignment[1..num_inputs];
+    let verified = verify_proof(&pvk, &proof, inputs).unwrap();
+
     assert!(verified);
 
-    c.bench_function("groth proof", |b| b.iter(|| {
-        prove(circom.clone(), &params, &mut rng).unwrap()
-    }));
+    c.bench_function("groth proof", |b| {
+        b.iter(|| {
+            black_box(create_proof_with_qap_and_matrices::<_, CircomReduction>(
+                &params,
+                r,
+                s,
+                &matrices,
+                num_inputs,
+                num_constraints,
+                full_assignment.as_slice(),
+            )
+            .unwrap());
+        })
+    });
 }
 
 criterion_group!(benches, groth);
