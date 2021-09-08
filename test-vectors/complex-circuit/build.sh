2f5cce376f274a9b73467305b01bfc2c3fd923f6
Kobi Gurkan <kobigurk@gmail.com>
2021-09-08 10:13:42 +0300

    fix: remove sym

--- test-vectors/complex-circuit/build.sh
+++ test-vectors/complex-circuit/build.sh
@@ -5,7 +5,7 @@ PTAU_POWER=$(echo "l($NUM_CONSTRAINTS)/l(2)" | bc -l | xargs -I{} awk "BEGIN{pri
 export NODE_OPTIONS=--max-old-space-size=8192
 
 echo "compiling"
-circom -f complex-circuit-$NUM_VARIABLES-$NUM_CONSTRAINTS.circom --r1cs --wasm --sym
+circom -f complex-circuit-$NUM_VARIABLES-$NUM_CONSTRAINTS.circom --r1cs --wasm
 
 echo "zkey"
 snarkjs zkey new complex-circuit-$NUM_VARIABLES-$NUM_CONSTRAINTS.r1cs powersOfTau28_hez_final_$PTAU_POWER.ptau complex-circuit-$NUM_VARIABLES-$NUM_CONSTRAINTS.zkey
