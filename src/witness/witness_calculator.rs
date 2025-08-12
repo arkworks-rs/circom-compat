76254066cd5bb108f51c4eb0628fdfdb7c92e649
Pratyush Mishra <pratyushmishra@berkeley.edu>
2025-08-11 18:56:18 -0700

    Work

--- src/witness/witness_calculator.rs
+++ src/witness/witness_calculator.rs
@@ -4,7 +4,6 @@ use color_eyre::Result;
 use num_bigint::BigInt;
 use num_traits::Zero;
 use wasmer::{Function, Instance, Memory, MemoryType, Module, RuntimeError, Store, imports};
-use wasmer_wasix::WasiEnv;
 
 use num::ToPrimitive;
 
@@ -82,9 +81,6 @@ impl WitnessCalculator {
         };
         let instance = Instance::new(store, &module, &import_object)?;
         let exports = instance.exports.clone();
-        let mut wasi_env = WasiEnv::builder("calculateWitness").finalize(store)?;
-        wasi_env.initialize(store, instance)?;
-        // wasi_env.initialize_with_memory(store, instance, Some(memory.clone()), false)?;
         let wasm = Wasm::new(exports, memory);
         Ok(wasm)
     }
